087be41c7d32104b95964095a724187310a5a36f
BrMD <dorigon.matheus8@gmail.com>
2024-02-14 11:08:34 -0300

    just an simple update

---
+++ backend/src/routes/routes.rs
@@ -0,0 +1,35 @@
+#![deny(warnings)]
+use std::net::SocketAddr;
+use bytes::{Buf, Bytes};
+use http_body_util::{BodyExt, Full};
+use hyper::server::conn::http1;
+use hyper::service::service_fn;
+use hyper::{body::Incoming as IncomingBody, header, Method, Request, Response, StatusCode};
+use tokio::net::{TcpListener, TcpStream};
+
+#[path = "../benches/support/mod.rs"]
+mod support;
+use support::TokioIo;
+
+static INDEX: &[u8] = b"<a href=\"test.html\">test.html</a>";
+static INTERNAL_SERVER_ERROR: &[u8] = b"Internal Server Error";
+static NOTFOUND: &[u8] = b"Not Found";
+static POST_DATA: &str = r#"{"original": "data"}"#;
+static URL: &str = "http://127.0.0.1:1337/json_api";
+
+type GenericError = Box<dyn std::error::Error + Send + Sync>;
+type Result<T> = std::result::Result<T, GenericError>;
+type BoxBody = http_body_util::combinators::BoxBody<Bytes, hyper::Error>;
+
+ async fn responses(req:Request<IncomingBody>) -> Result<Response<BoxBody>> {
+    match(req.method(), req.uri().path()){
+        (&Method::GET, "/") | (&Method::GET, "/index.html") => Ok(Response::new(full(INDEX))),
+        _ => {
+            // Return 404 not found response.
+            Ok(Response::builder()
+                .status(StatusCode::NOT_FOUND)
+                .body(full(NOTFOUND))
+                .unwrap())
+        }
+    }
+ }
