c6a9bf0a11c8c507c95a1d0f4ec6f167e4d81816
BrMD <dorigon.matheus8@gmail.com>
2024-02-24 10:43:49 -0300

    update

--- backend/src/database/database/database.module.ts
+++
@@ -1,5 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-// import { MyDataSource } from 'ormconfig';
-@Module({ imports: [TypeOrmModule.forRoot()] })
-export class DatabaseModule {}
--- backend/src/types/index.ts
+++
@@ -1,9 +0,0 @@
-export type prompTypes = 'emailVerifier' | 'emailSearch' | 'domainVerifier';
-
-export type prompt = { type: prompTypes; value: string };
-
-export enum promptTypesEnum {
-  emailVerifier = 'emailVerifier',
-  emailSearch = 'emailSearch',
-  domainVerifier = 'domainVerifier',
-}
